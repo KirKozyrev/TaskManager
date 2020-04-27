a7f5677a3a10420417ca96f6a1f674ecfaedfa86
KirKozyrev <kozyrev.k1995@gmail.com>
2020-04-27 17:52:35 +0400

    Add traits to task

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -4,7 +4,28 @@ FactoryBot.define do
     sequence(:description) {|n| "Description#{n}" }
     sequence(:author_id) {|n| n }
     sequence(:assignee_id) {|n| n }
-    sequence(:state) {|n| "State#{n}" }
     sequence(:expired_at) {|n| "#{Date.today + n}" }
+
+    trait :new_task do
+      state "new_task"
+    end
+    trait :in_development do
+      state "in_development"
+    end
+    trait :in_qa do
+      state "in_qa"
+    end
+    trait :in_code_review do
+      state "in_code_review"
+    end
+    trait :ready_for_release do
+      state "ready_for_release"
+    end
+    trait :released do
+      state "released"
+    end
+    trait :archived do
+      state "archived"
+    end
   end
 end
