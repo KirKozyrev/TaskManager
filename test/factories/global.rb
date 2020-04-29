353228bafcb947ba1ec553db0e9beed4eb78592a
KirKozyrev <kozyrev.k1995@gmail.com>
2020-04-29 17:42:28 +0400

    multiline sequences

--- test/factories/global.rb
+++ test/factories/global.rb
@@ -1,8 +1,20 @@
 FactoryBot.define do
-  sequence(:email, aliases: [:sender, :receiver]) { |n| "user#{n}@test.com" }
-  sequence(:name, aliases: [:first_name, :last_name]) { |n| "Name#{n}" }
-  sequence(:password) { |n| "Password#{n}" }
-  sequence(:avatar) { |n| "Avatar#{n}" }
-  sequence(:description) { |n| "Description#{n}" }
-  sequence(:expired_at) { |n| "#{Date.today + n}" }
+  sequence :email, aliases: [:sender, :receiver] do |n|
+    "user#{n}@test.com"
+  end
+  sequence :name, aliases: [:first_name, :last_name] do |n|
+    "Name#{n}"
+  end
+  sequence :password do |n|
+    "Password#{n}"
+  end
+  sequence :avatar do |n|
+    "Avatar#{n}"
+  end
+  sequence :description do |n|
+    "Description#{n}"
+  end
+  sequence :expired_at do |n|
+    "#{Date.today + n}"
+  end
 end
