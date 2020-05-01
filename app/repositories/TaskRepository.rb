5af7e4a3f679892ee24e9fd1fe7fd4f1a22b60f4
KirKozyrev <kozyrev.k1995@gmail.com>
2020-05-01 04:12:11 +0400

    edit code style

--- app/repositories/TaskRepository.rb
+++ app/repositories/TaskRepository.rb
@@ -1,32 +1,32 @@
 module TaskRepository
   extend ActiveSupport::Concern
 
-  state_machine :initial => :new_task do
+  state_machine initial: :new_task do
     event :to_development do
-      transition :new_task => :in_development
-      transition :in_qa => :in_development
-      transition :in_code_review => :in_development
+      transition new_task: :in_development
+      transition in_qa: :in_development
+      transition in_code_review: :in_development
     end
 
     event :to_qa do
-      transition :in_development => :in_qa
+      transition in_development: :in_qa
     end
 
     event :to_code_review do
-      transition :in_qa => :in_code_review
+      transition in_qa: :in_code_review
     end
 
     event :to_ready_for_release do
-      transition :in_code_review => :ready_for_release
+      transition in_code_review: :ready_for_release
     end
 
     event :to_released do
-      transition :ready_for_release => :released
+      transition ready_for_release: :released
     end
 
     event :to_archive do
-      transition :new_task => :archived
-      transition :released => :archived
+      transition new_task: :archived
+      transition released: :archived
     end
   end
 end
