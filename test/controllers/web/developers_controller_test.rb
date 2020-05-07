82d93f845d1edde28eda4355af3db677b4936b82
KirKozyrev <kozyrev.k1995@gmail.com>
2020-05-07 20:43:41 +0400

    edit create test

--- test/controllers/web/developers_controller_test.rb
+++ test/controllers/web/developers_controller_test.rb
@@ -9,5 +9,7 @@ class Web::DevelopersControllerTest < ActionController::TestCase
   test 'should post create' do
     post :create, params: { developer: attributes_for(:developer) }
     assert_response :redirect
+    follow_redirect!
+    assert_response :success
   end
 end
