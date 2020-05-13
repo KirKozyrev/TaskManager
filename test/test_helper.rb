5da6833e5b4fee952c0b6e47106f951fc0c65da5
KirKozyrev <kozyrev.k1995@gmail.com>
2020-05-13 11:43:48 +0400

    add AuthHelper to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+include AuthHelper
+
 class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
 
