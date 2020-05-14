45ffca0e42167c2eeb86a14bef9393cd22a8e0a5
KirKozyrev <kozyrev.k1995@gmail.com>
2020-05-14 11:31:46 +0400

    fix create test in developers controller

--- test/controllers/web/developers_controller_test.rb
+++ test/controllers/web/developers_controller_test.rb
@@ -7,9 +7,9 @@ class Web::DevelopersControllerTest < ActionController::TestCase
   end
 
   test 'should post create' do
-    post :create, params: { developer: attributes_for(:developer) }
-    assert_response :redirect
-    follow_redirect!
-    assert_response :success
+    new_user_attrs = attributes_for(:developer)
+    post :create, params: { developer: new_user_attrs }
+    new_user = User.find_by(first_name: new_user_attrs[:first_name])
+    assert_not new_user.nil?
   end
 end
