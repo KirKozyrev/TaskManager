3b2ae57dc3a6a3004739fd7c5ee545a670e80a9e
KirKozyrev <kozyrev.k1995@gmail.com>
2020-05-14 12:31:35 +0400

    edit tests in user controllers test

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -18,21 +18,28 @@ class Admin::UsersControllerTest < ActionController::TestCase
   end
 
   test "should post create" do
-    user = attributes_for(:user)
-    post :create, params: { user: user }
-    assert_response :redirect
+    user_attrs = attributes_for(:user)
+    post :create, params: { user: user_attrs }
+    new_user = User.find_by(first_name: user_attrs[:first_name])
+    assert_not new_user.nil?
   end
  
   test "should patch update" do
     user = create(:user)
     user_attrs = attributes_for(:user)
     patch :update, params: { id: user.id, user: user_attrs }
-    assert_response :redirect
+    searched_user = User.find(user.id);
+    user_attrs.each do |key, value|
+      unless (key.to_s == 'avatar' or key.to_s == 'password')
+        assert_equal value, searched_user[key]
+      end
+    end
   end
 
   test "should delete destroy" do
     user = create(:user)
     delete :destroy, params: {id: user.id }
-    assert_response :redirect
+    searched_user = User.where(id: user.id).take;
+    assert searched_user.nil?
   end
 end
