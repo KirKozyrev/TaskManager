2cc5f7c3cc4dacb71bd31f07bbb67081cb678285
KirKozyrev <kozyrev.k1995@gmail.com>
2020-05-18 15:08:32 +0400

    edit index action in api v1 tasks controller

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::TasksController < Api::V1::ApplicationController
-  def index; end
+  def index
+    tasks = Task.all.
+      ransack(ransack_params).
+      result.
+      page(page).
+      per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
+
   def show; end
+
   def create; end
+
   def update; end
+
   def destroy; end
 end
