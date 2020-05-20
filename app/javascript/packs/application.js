426b272322922d239f513a50b22e0dbb73b69852
KirKozyrev <kozyrev.k1995@gmail.com>
2020-05-20 11:58:15 +0400

    import webpacker-react and setup TaskBoard in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,3 +2,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'material-design-lite/material.js';
+import WebpackerReact from 'webpacker-react'
+import TaskBoard from './components/TaskBoard'
+
+WebpackerReact.setup({ TaskBoard })
