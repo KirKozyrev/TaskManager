f6030da01ffbcda29812a34175c3602750703230
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-03 13:58:01 +0400

    edit test_helper, add coveralls function

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,12 @@
+require 'coveralls'
+Coveralls.wear!('rails')
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
+  Capybara.default_max_wait_time = 25
   parallelize(workers: :number_of_processors)
 
   fixtures :all
