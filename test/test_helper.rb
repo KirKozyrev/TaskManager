f45f50de970c0a8220396f2440423b0c61bb88fe
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-03 19:23:59 +0400

    undo changes in test_helper and test.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,10 @@
 ENV['RAILS_ENV'] ||= 'test'
+
 require 'coveralls'
 Coveralls.wear!('rails')
+
 require_relative '../config/environment'
 require 'rails/test_help'
-require 'capybara'
-
-Capybara.configure do |config|
-  config.default_max_wait_time = 45
-end
 
 class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
