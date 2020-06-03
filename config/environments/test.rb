363b6ff7780f22728c06b9b7787fc44a2f2086e1
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-03 13:30:37 +0400

    configurate max wait time again

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -11,8 +11,8 @@ Rails.application.configure do
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
   # preloads Rails for running tests, you may have to set it to true.
-  config.eager_load = true
-  config.default_max_wait_time = 20
+  config.eager_load = false
+  config.default_max_wait_time = 40
 
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
