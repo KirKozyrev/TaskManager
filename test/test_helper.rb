633a02079421221412f1d63238d804372bafe04d
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-03 16:57:15 +0400

    edit test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,8 +3,9 @@ require 'coveralls'
 Coveralls.wear!('rails')
 require_relative '../config/environment'
 require 'rails/test_help'
+require 'Capybara'
 
-Capybara.default_max_wait_time = 35
+Capybara.default_max_wait_time = 45
 
 class ActiveSupport::TestCase
   parallelize(workers: :number_of_processors)
