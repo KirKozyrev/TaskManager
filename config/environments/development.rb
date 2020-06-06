9ee185665b5fa6c32f511731ca5b4e86d833ca19
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-06 10:46:09 +0400

    remove some Bullet configs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,16 +62,10 @@ Rails.application.configure do
 
   config.after_initialize do
     Bullet.enable = true
-    Bullet.sentry = true
     Bullet.alert = true
     Bullet.bullet_logger = true
     Bullet.console = true
-    Bullet.growl = true
     Bullet.rails_logger = true
-    Bullet.honeybadger = true
-    Bullet.bugsnag = true
-    Bullet.airbrake = true
-    Bullet.rollbar = true
     Bullet.add_footer = true
     Bullet.skip_html_injection = false
   end
