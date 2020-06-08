ed9d0def3267a951c522e5f17fd9b693294cd39d
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-08 14:42:20 +0400

    edit code style

--- test/controllers/admin/users_controller_test.rb
+++ test/controllers/admin/users_controller_test.rb
@@ -6,39 +6,39 @@ class Admin::UsersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     user = create(:user)
     get :edit, params: { id: user.id }
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should post create" do
+  test 'should post create' do
     user_attrs = attributes_for(:user)
     post :create, params: { user: user_attrs }
     new_user = User.find_by(email: user_attrs[:email])
     assert new_user.present?
   end
- 
-  test "should patch update" do
+
+  test 'should patch update' do
     user = create(:user)
     user_attrs = attributes_for(:user)
     patch :update, params: { id: user.id, user: user_attrs }
     searched_user = User.find(user.id)
     user_attrs.each do |key, value|
-      unless (key.to_s == 'avatar' or key.to_s == 'password')
+      unless (key.to_s == 'avatar') || (key.to_s == 'password')
         assert_equal value, searched_user[key]
       end
     end
   end
 
-  test "should delete destroy" do
+  test 'should delete destroy' do
     user = create(:user)
-    delete :destroy, params: {id: user.id }
+    delete :destroy, params: { id: user.id }
     assert_not User.exists?(id: user.id)
   end
 end
