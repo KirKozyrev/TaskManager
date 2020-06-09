1834a1c5a4d58b62cc893423f6b4de2d16d58d44
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-09 17:17:50 +0400

    edit update and destroy tests

--- test/controllers/api/v1/tasks_controller_test.rb
+++ test/controllers/api/v1/tasks_controller_test.rb
@@ -35,6 +35,7 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
 
   test 'should put update' do
     author = create(:user)
+    sign_in(author)
     assignee = create(:user)
     task = create(:task, author: author)
     task_attributes = attributes_for(:task).
@@ -54,8 +55,12 @@ class Api::V1::TasksControllerTest < ActionController::TestCase
 
   test 'should delete destroy' do
     author = create(:user)
+    sign_in(author)
     task = create(:task, author: author)
-    delete :destroy, params: { id: task.id, format: :json }
+
+    assert_emails 1 do
+      delete :destroy, params: { id: task.id, format: :json }
+    end
     assert_response :success
 
     assert !Task.where(id: task.id).exists?
