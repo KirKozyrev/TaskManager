32e23d80fb77d7ab28c9be61d757f60ac9453665
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-10 15:49:26 +0400

    add create action to password resets controller

--- app/controllers/web/password_resets_controller.rb
+++ app/controllers/web/password_resets_controller.rb
@@ -3,6 +3,24 @@ class Web::PasswordResetsController < Web::ApplicationController
     @password_reset = ResetForm.new
   end
 
+  def create
+    user = User.find_by(password_reset_params)
+    
+    if @user
+      user.create_reset_digest
+
+      UserMailer.with({ user: user }).password_reset.deliver_now
+
+      redirect_to root_url
+    end
+  end
+
   def edit
+  end
+
+  private
+
+  def password_reset_params
+    params.require(:reset_form).permit(:email)
   end
 end
