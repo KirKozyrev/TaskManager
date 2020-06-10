ddfb2d572251567f3327a91ee0746391f1959a10
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-10 18:28:51 +0400

    add update action to password resets controller

--- app/controllers/web/password_resets_controller.rb
+++ app/controllers/web/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class Web::PasswordResetsController < Web::ApplicationController
+  before_action :get_user,         only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
     @password_reset = ResetForm.new
   end
@@ -15,6 +18,19 @@ class Web::PasswordResetsController < Web::ApplicationController
     end
   end
 
+  def update
+    if params[@user_type][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update(user_params)
+      @user.reset_digest = nil
+      @user.save
+      redirect_to root_url
+    else
+      render 'edit'
+    end
+  end
+
   def edit
   end
 
@@ -22,5 +38,20 @@ class Web::PasswordResetsController < Web::ApplicationController
 
   def password_reset_params
     params.require(:reset_form).permit(:email)
+  end
+
+  def user_params
+    params.require(@user_type).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(reset_digest: User.encrypt(params[:id]))
+    @user_type = @user.type.downcase
+  end
+
+  def check_expiration
+    if @user.reset_sent_at < 24.hours.ago
+      redirect_to new_password_reset_url
+    end
   end
 end
