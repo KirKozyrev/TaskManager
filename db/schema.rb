944ee1919a41d6390fa8c5304bf1ae4e308a85f8
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-10 13:07:13 +0400

    add columns reset_digest and reset_sent_at to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_26_183410) do
+ActiveRecord::Schema.define(version: 2020_06_10_082606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2020_04_26_183410) do
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
   end
 
 end
