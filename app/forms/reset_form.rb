471a358d94dc539d9e87fc0e0117a1efcdc5f9a3
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-10 13:47:12 +0400

    add reset_form

---
+++ app/forms/reset_form.rb
@@ -0,0 +1,20 @@
+class ResetForm
+  include ActiveModel::Model
+
+  attr_accessor :email
+
+  validates :email, presence: true, format: { with: /\A\S+@.+\.\S+\z/ }
+  validate :user_valid?
+
+  def user
+    User.find_by(email: email)
+  end
+
+  private
+
+  def user_valid?
+    if user.blank?
+      errors.add(:email, "email doesn't match")
+    end
+  end
+end
