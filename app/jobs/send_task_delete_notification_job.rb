80e67e95f82db14760588307e7d0dca3bbf7a5df
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-16 13:52:52 +0400

    add uniqueness constraint to send jobs

--- app/jobs/send_task_delete_notification_job.rb
+++ app/jobs/send_task_delete_notification_job.rb
@@ -1,6 +1,7 @@
 class SendTaskDeleteNotificationJob < ApplicationJob
   sidekiq_options queue: :mailers
   sidekiq_throttle_as :mailer
+  sidekiq_options lock: :until_and_while_executing, on_conflict: { client: :log, server: :reject }
 
   def perform(task_id, task_author)
     user = User.find_by(id: task_author);
