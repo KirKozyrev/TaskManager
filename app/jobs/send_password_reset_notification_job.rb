ecccaf31f11b4250955022f2bcef359ffda02283
KirKozyrev <kozyrev.k1995@gmail.com>
2020-06-18 19:25:34 +0400

    change find_by to find

--- app/jobs/send_password_reset_notification_job.rb
+++ app/jobs/send_password_reset_notification_job.rb
@@ -4,7 +4,7 @@ class SendPasswordResetNotificationJob < ApplicationJob
   sidekiq_options lock: :until_and_while_executing, on_conflict: { client: :log, server: :reject }
 
   def perform(user_id, reset_token)
-    user = User.find_by(id: user_id);
+    user = User.find(user_id);
     return if user.blank?
     UserMailer.with(user: user, reset_token: reset_token).password_reset.deliver_now
   end
