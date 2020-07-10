059ed584645466562051568e48f1c09e321a80be
KirKozyrev <kozyrev.k1995@gmail.com>
2020-07-10 14:27:31 +0400

    add App.js; change entry point of Board

---
+++ app/javascript/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import store from 'store';
+import { Provider } from 'react-redux';
+
+import TaskBoardContainer from 'containers/TaskBoardContainer';
+import TaskBoard from 'components/TaskBoard';
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <TaskBoardContainer>
+        {({ board, loadBoard }) => <TaskBoard loadBoard={loadBoard} board={board} />}
+      </TaskBoardContainer>
+    </Provider>
+  );
+};
+
+export default App;
